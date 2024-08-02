679474e5440efe40850168551ba4e9375dbec9e3
samirk98 <samirkerimli84@gmail.com>
2024-08-02 20:24:19 +0400

    learning javascript to fullest

---
+++ script.js
@@ -0,0 +1,77 @@
+// "use strict";
+
+// num = 5;
+
+
+// letter = "one plus 4 is"
+
+// console.log(num, letter);   //cannot declare without variable with using strict
+
+
+// let $ = "samir"
+// let _ = "gulzar"
+
+// console.log(_);
+
+
+// const COLOR_RED = "#F00";
+// const COLOR_GREEN = "#0F0";
+// const COLOR_BLUE = "#00F";
+// const COLOR_ORANGE = "#FF7F00";
+
+// alert(COLOR_BLUE); //In other words, capital-named constants are only used as aliases for “hard-coded” values.
+
+// let admin, name;
+
+// name = "John";
+
+// admin = name;
+
+// alert(admin)
+
+
+// let n  = 123;
+
+// n = 123.5;
+
+// console.log(n);
+
+// let x = 150;
+
+// console.log(x / 0);
+
+
+
+// let x = 5n;
+// let y = x /2;
+
+// console.log(y);
+
+// let name = "Samir";
+
+// alert( `Hello, ${name}!`);
+// alert(`Hello the answer is ${1 + 2}!`)
+
+// let lesserThan = 1 > 5;
+
+// alert(lesserThan);
+
+// let name = "Ilya";
+
+// alert( `hello ${1}` ); // hello 1
+
+// alert( `hello ${"name"}` );  // hello name
+
+// alert( `hello ${name}` ); // hello Ilya
+
+// let age = prompt('How old are you ?');
+
+// alert(`You are ${age} years old!`)
+
+// let isBoss = confirm("Are you the boos?");
+
+// alert(isBoss)
+
+// let name = prompt("what is your name?");
+
+// alert(name)
